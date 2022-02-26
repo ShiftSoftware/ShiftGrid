77e32baaff402c2951345747a58dcdd5ad13f8d7
Aza Asim <azaasim@gmail.com>
2022-02-27 02:06:04 +0300

    Simplified Filter Operators

--- ShiftGrid.Core/GridFilterOperator.cs
+++ ShiftGrid.Core/GridFilterOperator.cs
@@ -2,12 +2,12 @@
 {
     public class GridFilterOperator
     {
-        public new const string Equals = "Equals";
-        public const string NotEquals = "NotEquals";
-        public const string GreaterThan = "GreaterThan";
-        public const string GreaterThanOrEquals = "GreaterThanOrEquals";
-        public const string LessThan = "LessThan";
-        public const string LessThanOrEquals = "LessThanOrEquals";
+        public new const string Equals = "=";
+        public const string NotEquals = "!=";
+        public const string GreaterThan = ">";
+        public const string GreaterThanOrEquals = ">=";
+        public const string LessThan = "<";
+        public const string LessThanOrEquals = "<=";
         public const string Contains = "Contains";
         public const string In = "In";
         public const string StartsWith = "StartsWith";
