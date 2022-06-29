65a31c1ab6f12055d804df86a84eefeab6d5928b
Saman <gtsaman@gmail.com>
2022-06-29 13:34:22 +0300

    Allow any origin

--- PlayGround/Program.cs
+++ PlayGround/Program.cs
@@ -4,6 +4,11 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
 app.MapControllers();
 
 app.Run();
