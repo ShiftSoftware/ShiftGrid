aac54f19ec051b5a14f55bd7174a8824fe117172
AzaAsim <aza@shift.software>
2022-07-09 17:31:34 +0300

    Change Summary to Aggregate on Postgress Tests

---
+++ Test.NETCore/Tests/Postgres/Aggregate.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.NETCore.Tests.Postgres
+{
+    [TestClass]
+    public class Aggregate : ShiftGrid.Test.Shared.Tests.Aggregate
+    {
+        public Aggregate() : base(typeof(EF.PostgresDB), new Utils(typeof(EF.PostgresDB)))
+        {
+
+        }
+    }
+}
