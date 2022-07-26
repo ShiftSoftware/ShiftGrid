3d4257af531783b8371cfc409a7a6db70cc0decf
Saman <gtsaman@gmail.com>
2022-07-26 15:32:55 +0300

    Add Swagger

--- PlayGround/Program.cs
+++ PlayGround/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var useNewtonsoftJson = false;
@@ -11,12 +13,25 @@ else
     builder.Services.AddControllers();
 }
 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ShiftGrid Playground",
+        Description = "",
+    });
+});
+
 var app = builder.Build();
 
 app.UseCors(builder => builder
     .AllowAnyOrigin()
     .AllowAnyMethod()
     .AllowAnyHeader());
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
